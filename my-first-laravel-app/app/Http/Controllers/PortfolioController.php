dca34d2842bbbdbc7cc201e9b2f3ec78eb4c87ee
user050507280221 <angelbenitezayado477@gmail.com>
2025-09-18 11:27:32 +0000

    main

--- my-first-laravel-app/app/Http/Controllers/PortfolioController.php
+++ my-first-laravel-app/app/Http/Controllers/PortfolioController.php
@@ -16,18 +16,22 @@ class PortfolioController extends Controller
             'bio' => 'An IT student passionate about web development and programming.',
             'profile_image' => 'https://lh3.googleusercontent.com/a/ACg8ocJWRFo9rigs8rfVMJcJ873JcDZ7Nq6tqf1bGTcwsTS3yh4uXHs=s360-c-no',
 
-            'skills' => ['Laravel', 'PHP', 'JavaScript', 'HTML5', 'CSS3'],
+            'skills' => ['Laravel', 'PHP', 'C#', 'HTML5', 'CSS3'],
 
-            'projects' => [
-                'Portfolio Website (HTML & CSS)' => [
-                    'details' => 'My first-year project that introduced me to web development. Built with HTML and CSS, focusing on layout, design, and responsive structure.',
-                    'link' => 'https://user050507280221.github.io/vn5e1_portfolio/index.html'
-                ],
-                'Photoshop Design Project' => [
-                    'details' => 'Created layouts, posters, and digital designs during activities and practice sessions, learning how to use Photoshop tools for web and graphic design.',
-                    'link' => 'https://lh3.googleusercontent.com/drive-storage/AJQWtBMp0t0srT0WKyL3G4ZFT0YRvo9S5ESJ8H1TBqMhINd00BB4QM9Bbz76-dbxvLqGQcUMeXsNmmTx98p9g2dqG4X0L2Zbwt-IbC1qSxXrlDoc66E=w1920-h912?auditContext=forDisplay'
-                ]
+           'projects' => [
+            'Portfolio Website (HTML & CSS)' => [
+                'details' => 'My first-year project that introduced me to web development. Built with HTML and CSS, focusing on layout, design, and responsive structure.',
+                'link' => 'https://user050507280221.github.io/vn5e1_portfolio/index.html'
             ],
+            'Calendar Project (HTML & CSS)' => [
+                'details' => 'A simple calendar application built with HTML and CSS. This project helped me practice layout, styling, and responsive design.',
+                'link' => 'https://user050507280221.github.io/vn5e1.//'
+            ],
+            'Photoshop Design Project' => [
+                'details' => 'Created layouts, posters, and digital designs during activities and practice sessions, learning how to use Photoshop tools for web and graphic design.',
+                'link' => 'https://lh3.googleusercontent.com/drive-storage/AJQWtBMp0t0srT0WKyL3G4ZFT0YRvo9S5ESJ8H1TBqMhINd00BB4QM9Bbz76-dbxvLqGQcUMeXsNmmTx98p9g2dqG4X0L2Zbwt-IbC1qSxXrlDoc66E=w1920-h912?auditContext=forDisplay'
+            ]
+        ],
 
             'experience' => [
                 'Student Assistant - IT Department' => 'Provided technical support, troubleshooting, and basic system maintenance while developing skills in IT operations.'
--- my-first-laravel-app/routes/web.php
+++ my-first-laravel-app/routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
 
-// Single-page portfolio route
 Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio.home');
 
-// Optional: redirect root to portfolio
 Route::get('/', function () {
     return redirect()->route('portfolio.home');
 });
